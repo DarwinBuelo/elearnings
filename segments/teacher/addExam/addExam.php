57e046b58da4307b48e9ef9da2630c13a68daacb
Darwin Buelo <dbuelo@gmail.com>
2019-09-01 10:32:09 +0800

    eod

--- segments/teacher/addExam/addExam.php
+++ segments/teacher/addExam/addExam.php
@@ -17,16 +17,26 @@ $courses =  Course::LoadArray();
                             <th>Course Name</th>
                             <th>Course Description</th>
                             <th>Units</th>
+                            <th>Lessons</th>
+                            <th>Exams</th>
                             <th>Option</th>
                         </tr>
                         </thead>
                         <tbody>
                         <?php
                         foreach ($courses as $course) {
+                            $countLessons = count($course->getLessons());
+                            $countExams = 0;
+                            foreach ($course->getLessons() as $lesson){
+                                $countExams += $lesson->getExams() != false ? count($lesson->getExams()) : 0 ;
+                            }
+
                             $html =  "<tr>";
                             $html .= "<td>{$course->getCourseName()}</td>";
                             $html .= "<td>{$course->getDesc()}</td>";
                             $html .= "<td>{$course->getUnits()}</td>";
+                            $html .= "<td>{$countLessons}</td>";
+                            $html .= "<td>{$countExams}</td>";
                             $html .= "<td>";
                             $backLink = urlencode($_SERVER['PHP_SELF'] ."?page=".Util::getParam('page'));
                             $html .="<a href='teacher.php?page=examDetails&cid={$course->getCourseID()}'>Add Exam</a>";
