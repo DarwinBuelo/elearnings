031e159f7bf0629f5a805959537e4a3c5f5fb1a3
Darwin Buelo <dbuelo@gmail.com>
2019-09-12 21:20:41 +0800

    Done fixing The Enrollment of course

--- class/Util.php
+++ class/Util.php
@@ -3,6 +3,12 @@
 class Util
 {
 
+    public static function dateNow(){
+        $dtz = new DateTimeZone("Asia/Hong_Kong"); //Your timezone
+        $now = new DateTime(date("Y-m-d"), $dtz);
+        return $now->format("Y-m-d H:i:s");
+    }
+
     public static function redirect($url)
     {
         ob_start();
--- segments/student/dashboard/dashboard.php
+++ segments/student/dashboard/dashboard.php
@@ -1,11 +1,41 @@
 <?php
 $Courses = Course::LoadArray();
 
+$User = unserialize($_SESSION['user']);
+$coursesEnrolled = $user->getCoursesEnrolled();
+foreach($coursesEnrolled as $key => $value ){
+    unset($Courses[$key]);
+}
+
 $html = '';
 $html .= "<div class='row'>";
 $html .= "<div class='col-md-12 text-center'><h3>Courses Offered</h3></div></div>";
 $html .= "<div class='row'><div class='col-md-12 text-center'></div></div><div class='row'><div class='col-md-12'>";
 echo $html;
+$html ='';
+$openHash = Util::getParam('message');
+$message = isset($_SESSION['message'][$openHash])?$_SESSION['message'][$openHash]:null;
+?>
+<?php
+ if (!empty($message) && $message['result'] == 'failed'): ?>
+    <div class="sufee-alert alert with-close alert-danger alert-dismissible fade show">
+        <span class="badge badge-pill badge-danger">Failed</span>
+        <?= $message['message'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
+    </div>
+<?php elseif (!empty($message) && $message['result'] == 'success'): ?>
+    <div class="sufee-alert alert with-close alert-success  alert-dismissible fade show">
+        <span class="badge badge-pill badge-success ">Success</span>
+        <?= $message['message'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
+    </div>
+<?php endif; ?>
+
+<?php
 foreach ($Courses as $course) {
     $fImg = !empty($course->getFeatureImage()) ? $course->getFeatureImage() : 'logo.png';
     ?>
@@ -30,7 +60,7 @@ foreach ($Courses as $course) {
                         Lessons
                     </li>
                     <li>
-                        <a href ="#" class="btn-sm btn-success">Enroll Now</a>
+                        <a href ="process.php?task=enrollToCourse&cid=<?= $course->getCourseID()?>" class="btn-sm btn-success">Enroll Now</a>
                     </li>
                 </ul>
             </div>
