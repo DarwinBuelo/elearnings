f5e11338893a216ec0fa00baa68f7be299f7b11f
Darwin Buelo <dbuelo@gmail.com>
2019-10-17 21:40:15 +0800

    EOD Commit

---
+++ segments/teacher/uploadFiles/uploadFiles.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-md-12 text-center m-2">
+        <h3>Uploaded Files</h3>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12 text-center"><button class="btn btn-danger">Upload File</button></div>
+</div>
+
+<!-- List of uploaded Files -->
+<div class="row">
+    <div class="col-md-12">
+        This will be the list of files on the folders
+    </div>
+</div>
